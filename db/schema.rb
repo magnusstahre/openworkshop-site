f4dd2733ed5264f30ba587ebb6b4a9a2affcbe1f
Cory Flanigan <seeflanigan@gmail.com>
2009-07-07 21:32:16 -0400

    Added missing files

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090708011617) do
+
+  create_table "workshops", :force => true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "date_and_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "country"
+  end
 
 end
